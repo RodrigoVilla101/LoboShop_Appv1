773cf262b4009c49ef9bf7127ecb847c3ccded47
RodrigoVilla101 <rodrigovillalpando5@hotmail.com>
2025-12-04 23:22:46 -0600

    chore: Enable CapacitorHttp plugin.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,6 +12,9 @@ const config: CapacitorConfig = {
   }, 
   */
   plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
     SplashScreen: {
       launchShowDuration: 2000,
       backgroundColor: '#3880ff',
